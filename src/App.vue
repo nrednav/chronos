aef1cc8307dd5a3dee4a3112d09071b117946369
nrednav <nrednav@gmail.com>
2019-11-29 16:20:31 +0400

    Add color scheme variables

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,29 @@
 </template>
 
 <style lang="less">
+// Default color scheme
+@black: #282c34;
+@white: #abb2bf;
+@light-red: #e06c75;
+@dark-red: #be5046;
+@green: #98c379;
+@light-yellow: #e5c07b;
+@orange: #d19a66;
+@blue: #61afef;
+@magenta: #c678dd;
+@cyan: #56b6c2;
+@gutter-grey: #4b5263;
+@comment-grey: #5c6370;
+@background: white;
+
+.dark-theme {
+  background: @black;
+}
+
 body {
   margin: 0;
   padding: 0;
-  background: #282c34;
+  background: @background;
 }
 
 #app {
