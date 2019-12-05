3aaa6f08c323177853c663704e20fdded0d125b5
nrednav <nrednav@gmail.com>
2019-12-05 17:02:12 +0400

    Define layout for main component

--- src/features/Main.vue
+++ src/features/Main.vue
@@ -1,24 +1,93 @@
 <template>
-  <div id="app-main">
-    Main
-    <br />
-    <router-link to="/">Home</router-link>
-    <br />
-    <router-link to="/stats">Stats</router-link>
-    <br />
-    <router-link to="/settings">Settings</router-link>
+  <div class="main">
+    <div class="main__header">
+      <div class="button--home"></div>
+      <div class="main__scramble"></div>
+      <div class="main__scramble-button"></div>
+    </div>
+    <div class="main__timer">
+      <div class="main__timer-clock"></div>
+      <div class="main__timer-control"></div>
+      <div class="main__timer-warmup" v-if="timerStarted"></div>
+    </div>
+    <div class="main__stats">
+      <div class="main__stats-avgOf5"></div>
+      <div class="main__stats-best"></div>
+      <div class="button--statistics"></div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      timerStarted: false
+    };
+  }
+};
 </script>
 
 <style lang="less">
 @import "../assets/styles/colors.less";
 
-#app-settings {
+.main {
   width: 100%;
   height: 100%;
+
+  display: grid;
+  grid-template-rows: 20% 60% 20%;
+
+  &__header {
+    background: var(--light-red);
+    display: grid;
+    grid-template-columns: 16% 68% 16%;
+  }
+
+  &__scramble {
+    border: 1px solid white;
+
+    &-button {
+      border: 1px solid white;
+    }
+  }
+
+  &__timer {
+    background: var(--green);
+    display: grid;
+    grid-template-rows: 70% 30%;
+
+    &-clock {
+      border: 1px solid white;
+    }
+
+    &-control {
+      border: 1px solid white;
+    }
+
+    &-warmup {
+    }
+  }
+
+  &__stats {
+    background: var(--blue);
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+
+    &-avgOf5 {
+      border: 1px solid white;
+    }
+
+    &-best {
+      border: 1px solid white;
+    }
+  }
+}
+
+.button--home {
+  border: 1px solid white;
+}
+
+.button--statistics {
 }
 </style>
