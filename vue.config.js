ff88874c61af6c72bc58e2ecf8780c2011c2f9eb
nrednav <nrednav@gmail.com>
2019-12-19 22:16:19 +0400

    Add builder options to vue config for app distribution Add application icons

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
 // vue.config.js
 module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: "com.nrednav.chronos",
+        directories: {
+          buildResources: "build"
+        },
+        mac: {
+          target: "dmg"
+        },
+        win: {
+          target: "portable"
+        }
+      }
+    }
+  },
   configureWebpack: {
     module: {
       rules: [
